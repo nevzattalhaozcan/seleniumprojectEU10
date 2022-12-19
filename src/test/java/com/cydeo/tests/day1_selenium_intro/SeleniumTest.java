a15840fa9233290dd15a20320df926cdd93c79b9
Nevzat Talha Ozcan <ozcann.talha@gmail.com>
2022-12-19 15:54:27 +0300

    Updated SeleniumTest.java

--- src/test/java/com/cydeo/tests/day1_selenium_intro/SeleniumTest.java
+++ src/test/java/com/cydeo/tests/day1_selenium_intro/SeleniumTest.java
@@ -9,6 +9,7 @@ public class SeleniumTest {
     public static void main(String[] args) {
 
         // 1- Setting up the web driver manager
+        // We create our "Browser Driver"
         WebDriverManager.chromedriver().setup();
 
         // 2- Create instance of Chrome driver
